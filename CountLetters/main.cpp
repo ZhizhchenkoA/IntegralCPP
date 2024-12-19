840d7f6ffa1f484a4a214c7eaf2a86e88165afbf
ZhizhchenkoA <sashazh@yahoo.com>
2024-12-19 22:59:47 +0300

    added count letters

---
+++ CountLetters/main.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+
+
+
+int main() {
+    
+
+
+    FILE *fp_r = fopen("input.txt", "r");
+    FILE *fp_w = fopen("output.txt", "w+");
+    int *arr_eng = (int*) calloc(26, sizeof(int));
+   
+    char c;
+
+    while ((c = fgetc(fp_r)) != EOF)
+        if (c >= 'a' && c <= 'z')
+            arr_eng[c - 'a']++;
+        else if (c >= 'A' && c <= 'Z')
+            arr_eng[c - 'A']++;
+        
+    for (int i = 0; i < 26; i++)
+        if (arr_eng[i] != 0)
+            fprintf(fp_w, "%c: %d\n", (char) (i) + 'a', arr_eng[i]);
+    
+   
+
+    fclose(fp_r);
+    fclose(fp_w);
+    free(arr_eng);
+    return 0;
+}
